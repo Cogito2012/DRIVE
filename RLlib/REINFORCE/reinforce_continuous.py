9cfd65ca9b71c45425bb7314d2b635b37c9daf42
baowentao <wtbao2018@gmail.com>
2020-08-10 11:18:16 -0400

    update REINFORCE

--- RLlib/REINFORCE/reinforce_continuous.py
+++ RLlib/REINFORCE/reinforce_continuous.py
@@ -12,6 +12,9 @@ import torchvision.transforms as T
 from torch.autograd import Variable
 
 pi = Variable(torch.FloatTensor([math.pi])).cuda()
+LOG_SIG_MAX = 2
+LOG_SIG_MIN = -20
+
 
 def normal(x, mu, sigma_sq):
     a = (-1*(Variable(x)-mu).pow(2)/(2*sigma_sq)).exp()
@@ -31,59 +34,120 @@ class Policy(nn.Module):
     def forward(self, inputs):
         x = inputs
         x = F.relu(self.linear1(x))
-        x = self.linear_mu(x)
-        mu_fix = torch.tanh(x[:, :2])  # (-1, 1)
-        mu_score = x[:, 2:]
-        # sigma_sq = F.softplus(self.linear_sigma(x)) + 1e-6
+        mu = self.linear_mu(x)
+        sigma_sq = self.linear_sigma(x)
+        sigma_sq = torch.clamp(sigma_sq, min=2 * LOG_SIG_MIN, max=2 * LOG_SIG_MAX)
 
-        return mu_fix, mu_score
+        return mu, sigma_sq
 
 
 class REINFORCE:
-    def __init__(self, hidden_size, num_inputs, num_outputs, device=torch.device("cuda")):
-        self.policy_model = Policy(hidden_size, num_inputs, num_outputs)
+    def __init__(self, num_inputs, dim_action, cfg, device=torch.device("cuda")):
+        self.policy_model = Policy(cfg.hidden_size, num_inputs, dim_action)
         self.device = device
         self.policy_model = self.policy_model.to(device)
         self.optimizer = optim.Adam(self.policy_model.parameters(), lr=1e-3)
+        self.num_classes = cfg.num_classes
+        # self.ce_loss = torch.nn.CrossEntropyLoss(reduction='none')
+        self.nll_loss = torch.nn.NLLLoss(reduction='none')
         
 
-    def select_action(self, state):
+    def select_action(self, state, with_grad=False):
         """
         state: (1, 128)
         """
         state = Variable(torch.from_numpy(state)).to(self.device)
-        fix_scale, accident_score = self.policy_model(state)
-        mu = torch.cat([fix_scale, accident_score], dim=1)
-        sigma_sq = Variable(torch.zeros_like(mu).to(self.device) + 1e-6)
+        mu, sigma_sq = self.policy_model(state)
+        sigma_sq = F.softplus(sigma_sq)
 
         eps = Variable(torch.randn(mu.size())).to(self.device)
-        # calculate the probability
-        action = (mu + sigma_sq.sqrt()*eps).data
-        # # clip the action to (-1, 1)
-        # action[:, :2] = torch.clamp(action[:, :2], -1, 1)
-        # # action[2:] = torch.softmax(action[2:], dim=0)
+        x_t = mu + sigma_sq.sqrt()*eps  # reparameterization trick
+        # compute action
+        action = torch.tanh(x_t)
 
-        prob = normal(action, mu, sigma_sq)
-        entropy = -0.5*((sigma_sq+2*pi.expand_as(sigma_sq)).log()+1)
+        # compute logPI
+        prob = normal(x_t.data, mu, sigma_sq)
         log_prob = prob.log()
-        # entropy = torch.zeros_like(action).to(self.device)
-        # log_prob = torch.ones_like(action).to(self.device)
+        # Enforcing Action Bound
+        log_prob -= torch.log(1 - action.pow(2) + 1e-6)
+        log_prob = log_prob.sum(1, keepdim=True)
 
-        return action[0].detach().cpu().numpy(), log_prob[0], entropy[0]
+        # compute entropy
+        entropy = -0.5*((sigma_sq + 2*pi.expand_as(sigma_sq)).log() + 1)
+
+        action_out = action if with_grad else action[0].detach().cpu().numpy()
+        return action_out, log_prob, entropy
 
 
-    def update_parameters(self, rewards, tta_losses, log_probs, entropies, gamma, alpha):
+    def update_parameters(self, rewards, log_probs, entropies, states, labels, cfg):
         R = torch.zeros(1, 1)
-        loss = 0
-        for i in reversed(range(len(rewards))):
-            R = gamma * R + rewards[i]
-            loss = loss - (log_probs[i]*(Variable(R).expand_as(log_probs[i])).to(self.device)).sum() - (alpha * entropies[i]).sum() + 0.01 * tta_losses[i]
-        loss = loss / len(rewards)
+        policy_loss = 0
+        horizon = len(rewards)
+        actions = []
+        for i in reversed(range(horizon)):
+            # compute discounted return
+            R = cfg.gamma * R + rewards[i]
+            # get the action with grad
+            # state = Variable(torch.from_numpy(states[i-horizon+1])).to(self.device)
+            state = states[i-horizon+1]
+            action, _, _ = self.select_action(state, with_grad=True)
+            actions.append(action)
+            # total loss
+            policy_loss = policy_loss - (log_probs[i]*(Variable(R).expand_as(log_probs[i])).to(self.device)).sum() - (cfg.alpha * entropies[i]).sum()
+        policy_loss = policy_loss / len(rewards)
 		
+        # compute time-to-accident losses
+        task_loss = self.compute_task_loss(actions, labels)
+        total_loss = cfg.beta * policy_loss + task_loss
+
         self.optimizer.zero_grad()
-        loss.backward()
+        total_loss.backward()
         utils.clip_grad_norm_(self.policy_model.parameters(), 40)
         self.optimizer.step()
+
+        return total_loss, policy_loss, task_loss
+
+
+    def compute_task_loss(self, actions, labels):
+        """Compute the loss for accident anticipation
+        """
+        batch_size = labels.shape[0]
+        # get prediction
+        actions = torch.cat(actions, dim=0)  # T x 3
+        score_pred = 0.5 * (actions[:, 2] + 1.0)
+        # get label target
+        labels = torch.from_numpy(labels).to(self.device)
+        steps, clsID, toa, fps = labels[:, 0], labels[:, 1], labels[:, 2], labels[:, 3]
+        task_target = torch.zeros(batch_size, self.num_classes).to(self.device)
+        task_target.scatter_(1, clsID.unsqueeze(1).long(), 1)  # one-hot
+        # compute loss
+        task_loss = self._exp_loss(score_pred, task_target, steps / fps, toa)
+
+        return task_loss
+
+    
+    def _exp_loss(self, pred, target, time, toa):
+        '''
+        :param pred:
+        :param target: onehot codings for binary classification
+        :param time:
+        :param toa:
+        :return:
+        '''
+        pred = torch.cat([(1.0 - pred).unsqueeze(1), pred.unsqueeze(1)], dim=1)
+        # positive example (exp_loss)
+        target_cls = target[:, 1]
+        target_cls = target_cls.to(torch.long)
+        
+        penalty = -torch.max(torch.zeros_like(toa).to(toa.device, pred.dtype), toa.to(pred.dtype) - time - 1)
+        penalty = torch.where(toa > 0, penalty, torch.zeros_like(penalty).to(pred.device))
+
+        # pos_loss = -torch.mul(torch.exp(penalty), -self.ce_loss(pred, target_cls))
+        pos_loss = -torch.mul(torch.exp(penalty), -self.nll_loss(torch.log(pred + 1e-6), target_cls))
+        # negative example
+        # neg_loss = self.ce_loss(pred, target_cls)
+        neg_loss = self.nll_loss(torch.log(pred + 1e-6), target_cls)
+        loss = torch.mean(torch.add(torch.mul(pos_loss, target[:, 1]), torch.mul(neg_loss, target[:, 0])))
         return loss
 
 
--- main_reinforce.py
+++ main_reinforce.py
@@ -1,5 +1,7 @@
 import argparse, math, os
 import numpy as np
+import yaml
+from easydict import EasyDict
 from src.enviroment import DashCamEnv
 from RLlib.REINFORCE.reinforce_continuous import REINFORCE
 
@@ -12,49 +14,33 @@ from src.data_transform import ProcessImages, ProcessFixations
 from tqdm import tqdm
 import datetime
 from torch.utils.tensorboard import SummaryWriter
-from src.eval_tools import evaluation_dynamic
+from metrics.eval_tools import evaluation_accident, evaluation_fixation
 
 
 def parse_main_args():
     parser = argparse.ArgumentParser(description='PyTorch REINFORCE implementation')
-    # For data loading
-    parser.add_argument('--data_path', default='./data/DADA-2000',
-                        help='The relative path of dataset.')
-    parser.add_argument('--batch_size', type=int, default=1,
-                        help='The batch size in training process. Default: 1')
-    parser.add_argument('--input_shape', nargs='+', type=int, default=[480, 640],
-                        help='The input shape of images. default: [r=480, c=640]')
-    parser.add_argument('--frame_interval', type=int, default=5,
-                        help='The number of frames per second for each video. Default: 10')
-    parser.add_argument('--max_frames', default=-1, type=int,
-                        help='Maximum number of frames for each untrimmed video. Default: -1 (all frames)')
-    parser.add_argument('--binary_cls', action='store_true',
-                        help='Whether to use binary accident prediction [1: ego, 0: non-ego]')
-    parser.add_argument('--num_workers', type=int, default=0, 
-                        help='How many sub-workers to load dataset. Default: 0')
     # For training and testing
+    parser.add_argument('--config', default="cfgs/reinforce_default.yml",
+                        help='Configuration file for SAC algorithm.')
     parser.add_argument('--phase', default='test', choices=['train', 'test'],
                         help='Training or testing phase.')
-    parser.add_argument('--env_model', default='./output/saliency/checkpoints/saliency_model_25.pth',
-                        help='The model weight file of environment model.')
-    parser.add_argument('--dim_action', type=int, default=4, 
-                        help='The dimension of action space. Default: 3')
-    parser.add_argument('--gamma', type=float, default=0.99, metavar='G',
-                        help='discount factor for reward (default: 0.99)')
-    parser.add_argument('--alpha', type=float, default=0.0001,
-                        help='The coefficient of entropy term of the loss.')
     parser.add_argument('--seed', type=int, default=123, metavar='N',
                         help='random seed (default: 123)')
-    parser.add_argument('--gpus', type=str, default="0", 
-                        help="The delimited list of GPU IDs separated with comma. Default: '0'.")
     parser.add_argument('--num_epoch', type=int, default=50, metavar='N',
                         help='number of epoches (default: 50)')
-    parser.add_argument('--hidden_size', type=int, default=128, metavar='N',
-                        help='number of episodes (default: 128)')
-    parser.add_argument('--output', default='./output/REINFORCE-TEMP',
+    parser.add_argument('--test_epoch', type=int, default=-1, 
+                        help='The snapshot id of trained model for testing.')
+    parser.add_argument('--output', default='./output/REINFORCE',
                         help='Directory of the output. ')
     args = parser.parse_args()
-    return args
+
+    with open(args.config, 'r') as f:
+        cfg = EasyDict(yaml.load(f))
+    cfg.update(vars(args))
+    device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
+    cfg.update(device=device)
+
+    return cfg
 
 
 def set_deterministic(seed):
@@ -68,167 +54,212 @@ def set_deterministic(seed):
     torch.backends.cudnn.deterministic = True
 
 
-def setup_dataloader(input_shape, output_shape, isTraining=True):
+def setup_dataloader(cfg, isTraining=True):
 
     img_shape = [660, 1584]
-    transform_dict = {'image': transforms.Compose([ProcessImages(input_shape)]),
-                      'focus': transforms.Compose([ProcessImages(output_shape)]), 
-                      'fixpt': transforms.Compose([ProcessFixations(input_shape, img_shape)])}
+    transform_dict = {'image': transforms.Compose([ProcessImages(cfg.input_shape)]),
+                      'focus': transforms.Compose([ProcessImages(cfg.output_shape)]), 
+                      'fixpt': transforms.Compose([ProcessFixations(cfg.input_shape, cfg.image_shape)])}
     params_norm = {'mean': [0.485, 0.456, 0.406], 'std': [0.229, 0.224, 0.225]}
 
     # testing dataset
     if not isTraining:
-        test_data = DADALoader(args.data_path, 'testing', interval=1, max_frames=-1, 
-                                transforms=transform_dict, params_norm=params_norm, binary_cls=args.binary_cls)
-        testdata_loader = DataLoader(dataset=test_data, batch_size=1, shuffle=False, num_workers=args.num_workers)
+        test_data = DADALoader(cfg.data_path, 'testing', interval=1, max_frames=-1, 
+                                transforms=transform_dict, params_norm=params_norm, binary_cls=cfg.binary_cls, use_focus=cfg.use_salmap)
+        testdata_loader = DataLoader(dataset=test_data, batch_size=1, shuffle=False, num_workers=0)
         print("# test set: %d"%(len(test_data)))
         return testdata_loader
 
     # training dataset
-    train_data = DADALoader(args.data_path, 'training', interval=args.frame_interval, max_frames=args.max_frames, 
-                            transforms=transform_dict, params_norm=params_norm, binary_cls=args.binary_cls)
-    traindata_loader = DataLoader(dataset=train_data, batch_size=args.batch_size, shuffle=True, num_workers=args.num_workers)
+    train_data = DADALoader(cfg.data_path, 'training', interval=cfg.frame_interval, max_frames=cfg.max_frames, 
+                            transforms=transform_dict, params_norm=params_norm, binary_cls=cfg.binary_cls, use_focus=cfg.use_salmap)
+    traindata_loader = DataLoader(dataset=train_data, batch_size=cfg.batch_size, shuffle=True, num_workers=cfg.num_workers)
     # validataion dataset
-    eval_data = DADALoader(args.data_path, 'validation', interval=args.frame_interval, max_frames=args.max_frames, 
-                            transforms=transform_dict, params_norm=params_norm, binary_cls=args.binary_cls)
-    evaldata_loader = DataLoader(dataset=eval_data, batch_size=args.batch_size, shuffle=False, num_workers=args.num_workers)
+    eval_data = DADALoader(cfg.data_path, 'validation', interval=cfg.frame_interval, max_frames=cfg.max_frames, 
+                            transforms=transform_dict, params_norm=params_norm, binary_cls=cfg.binary_cls, use_focus=cfg.use_salmap)
+    evaldata_loader = DataLoader(dataset=eval_data, batch_size=cfg.batch_size, shuffle=False, num_workers=cfg.num_workers)
     print("# train set: %d, eval set: %d"%(len(train_data), len(eval_data)))
+
     return traindata_loader, evaldata_loader
+
+
+def train_per_epoch(traindata_loader, env, agent, cfg, writer, epoch):
+    # we define each episode as the entire database
+    reward_total = 0
+    for i, (video_data, _, coord_data, data_info) in tqdm(enumerate(traindata_loader), 
+                                                    total=len(traindata_loader), desc='Training Epoch: %d / %d'%(epoch + 1, cfg.num_epoch)):  # (B, T, H, W, C)
+        state = env.set_data(video_data, coord_data)
+        entropies, log_probs, rewards, states, labels = [], [], [], [], []
+        done = False
+        # run each time step
+        while not done:
+            states.append(state)
+            # select action
+            action, log_prob, entropy = agent.select_action(state)
+            # state transition
+            state, reward, done, info = env.step(action)
+            
+            # gather info
+            entropies.append(entropy)
+            log_probs.append(log_prob)
+            rewards.append(reward)
+            labels.append(np.array([env.cur_step-1, env.clsID-1, env.begin_accident, env.fps], dtype=np.float32))
+
+        labels = np.array(labels, dtype=np.float32)
+        total_loss, policy_loss, task_loss = agent.update_parameters(rewards, log_probs, entropies, states, labels, cfg.REINFORCE)
+
+        avg_reward = np.sum(rewards) / len(rewards)
+        reward_total += np.sum(rewards)
+
+        i_episode = epoch * len(traindata_loader) + i
+        writer.add_scalar('loss/total_loss', total_loss, i_episode)
+        writer.add_scalar('loss/policy_loss', policy_loss, i_episode)
+        writer.add_scalar('loss/task_loss', task_loss, i_episode)
+        writer.add_scalar('reward/train_per_video', avg_reward, i_episode)
+
+    writer.add_scalar('reward/train_per_epoch', reward_total, epoch)
+
+
+def eval_per_epoch(evaldata_loader, env, agent, cfg, writer, epoch):
+
+    total_reward = 0
+    for i, (video_data, _, coord_data, data_info) in tqdm(enumerate(evaldata_loader), 
+                                                        total=len(evaldata_loader), desc='Testing Epoch: %d / %d'%(epoch + 1, cfg.num_epoch)):  # (B, T, H, W, C)
+        # set environment data
+        state = env.set_data(video_data, coord_data)
+        rewards = []
+        done = False
+        while not done:
+            # select action
+            action, log_prob, entropy = agent.select_action(state)
+            # state transition
+            state, reward, done, info = env.step(action)
+            rewards.append(reward)
+
+        avg_reward = np.sum(rewards) / len(rewards)
+        total_reward += np.sum(rewards)
+
+        i_episode = epoch * len(evaldata_loader) + i
+        writer.add_scalar('reward/test_per_video', avg_reward, i_episode)
+    writer.add_scalar('reward/test_per_epoch', total_reward, epoch)
 
 
 def train():
     # initilize environment
-    env = DashCamEnv(args.input_shape, args.dim_action, fps=30/args.frame_interval, device=device)
-    env.set_model(pretrained=True, weight_file=args.env_model)
+    env = DashCamEnv(cfg.ENV, device=cfg.device)
+    env.set_model(pretrained=True, weight_file=cfg.ENV.env_model)
+    cfg.ENV.output_shape = env.output_shape
 
     # prepare output directory
-    ckpt_dir = os.path.join(args.output, 'checkpoints')
+    ckpt_dir = os.path.join(cfg.output, 'checkpoints')
     if not os.path.exists(ckpt_dir):
         os.makedirs(ckpt_dir)
-    #Tesnorboard
-    writer = SummaryWriter(args.output + '/tensorboard/train_{}'.format(datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")))
-
-    # initialize agents
-    agent = REINFORCE(args.hidden_size, env.dim_state, env.dim_action, device=device)
-    agent.policy_model.train()
+    #Tesnorboard 
+    writer = SummaryWriter(cfg.output + '/tensorboard/train_{}'.format(datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")))
 
     # initialize dataset
-    traindata_loader, evaldata_loader = setup_dataloader(args.input_shape, env.output_shape)
+    traindata_loader, evaldata_loader = setup_dataloader(cfg.ENV)
+
+    # initialize agents
+    agent = REINFORCE(env.dim_state, env.dim_action, cfg.REINFORCE, device=cfg.device)
     
     num_episode = 0
-    for e in range(args.num_epoch):
-        # we define each episode as the entire database
-        for i, (video_data, focus_data, coord_data, data_info) in tqdm(enumerate(traindata_loader), total=len(traindata_loader), desc='Epoch: %d / %d'%(e + 1, args.num_epoch)):  # (B, T, H, W, C)
-            state = env.set_data(video_data, focus_data, coord_data)
-            entropies, log_probs, rewards, cls_losses = [], [], [], []
-            # run each time step
-            for t in range(env.max_step):
-                # select action
-                action, log_prob, entropy = agent.select_action(state)
-                # state transition
-                state, reward, cls_loss, done, _ = env.step(action, with_loss=True)
-                
-                # gather info
-                entropies.append(entropy)
-                log_probs.append(log_prob)
-                rewards.append(reward)
-                cls_losses.append(cls_loss)
-                if done:
-                    break
-
-            num_episode += 1
-            loss = agent.update_parameters(rewards, cls_losses, log_probs, entropies, args.gamma, args.alpha)
-
-            avg_reward = np.sum(rewards) / len(rewards)
-            avg_cls_loss = np.sum(cls_losses) / len(cls_losses)
-            writer.add_scalar('loss/total_loss', loss, num_episode)
-            writer.add_scalar('loss/cls_loss', avg_cls_loss, num_episode)
-            writer.add_scalar('reward/avg_reward', avg_reward, num_episode)
-            print("Episode: %d, avg reward: %.3f, avg_cls_loss: %.3f, total los: %.3f"%(num_episode, avg_reward, avg_cls_loss, loss))
+    for e in range(cfg.num_epoch):
+        # train each epoch
+        agent.policy_model.train()
+        train_per_epoch(traindata_loader, env, agent, cfg, writer, e)
 
         # save model file for each epoch (episode)
         torch.save(agent.policy_model.state_dict(), os.path.join(ckpt_dir, 'reinforce_epoch_%02d.pth'%(e+1)))
+
+        # evaluate each epoch
+        agent.policy_model.eval()
+        with torch.no_grad():
+            eval_per_epoch(evaldata_loader, env, agent, cfg, writer, e)
+
+    writer.close()
     env.close()
 
 
 def test():
     # prepare output directory
-    output_dir = os.path.join(args.output, 'eval')
+    output_dir = os.path.join(cfg.output, 'eval')
     if not os.path.exists(output_dir):
         os.makedirs(output_dir)
     
     result_file = os.path.join(output_dir, 'results.npy')
     if os.path.exists(result_file):
-        save_dict = np.load(result_file)
+        save_dict = np.load(result_file, allow_pickle=True)
         save_dict = save_dict.item()
     else:
         # initilize environment
-        env = DashCamEnv(args.input_shape, args.dim_action, fps=30/args.frame_interval, device=device)
-        env.set_model(pretrained=True, weight_file=args.env_model)
-
-        # initialize agents
-        agent = REINFORCE(args.hidden_size, env.dim_state, env.dim_action, device=device)
-        agent.policy_model.eval()
+        env = DashCamEnv(cfg.ENV, device=cfg.device)
+        env.set_model(pretrained=True, weight_file=cfg.ENV.env_model)
+        cfg.ENV.output_shape = env.output_shape
 
         # initialize dataset
-        testdata_loader = setup_dataloader(args.input_shape, env.output_shape, isTraining=False)
+        testdata_loader = setup_dataloader(cfg.ENV, isTraining=False)
+
+        # initialize agents
+        agent = REINFORCE(env.dim_state, env.dim_action, cfg.REINFORCE, device=cfg.device)
 
         # load agent models (by default: the last epoch)
-        ckpt_dir = os.path.join(args.output, 'checkpoints')
-        agent.load_models(ckpt_dir, args)
-
-        save_dict = {'all_preds': [], 'gt_labels': [], 'toas': [], 'vids': []}
-        for i, (video_data, focus_data, coord_data, data_info) in tqdm(enumerate(testdata_loader), total=len(testdata_loader)):  # (B, T, H, W, C)
+        ckpt_dir = os.path.join(cfg.output, 'checkpoints')
+        agent.load_models(ckpt_dir, cfg)
+        
+        agent.policy_model.eval()
+        save_dict = {'score_preds': [], 'fix_preds': [], 'gt_labels': [], 'gt_fixes': [], 'toas': [], 'vids': []}
+        for i, (video_data, _, coord_data, data_info) in tqdm(enumerate(testdata_loader), total=len(testdata_loader)):  # (B, T, H, W, C)
             # set environment data
-            state = env.set_data(video_data, focus_data, coord_data)
+            state = env.set_data(video_data, coord_data)
 
-            pred_scores = []
-            # run each time step
-            for t in range(env.max_step):
+            done = False
+            pred_scores, pred_fixes = [], []
+            while not done:
                 # select action
                 action, _, _ = agent.select_action(state)
                 # state transition
                 state, reward, done, _ = env.step(action)
-
-                accident_pred = action[2:]
-                score = np.exp(accident_pred[1]) / np.sum(np.exp(accident_pred))
+                # gather action results
+                next_fixation = env.pred_to_point(action[0], action[1])
+                score = 0.5 * (action[2] + 1.0)
+                pred_fixes.append(next_fixation)
                 pred_scores.append(score)
+            
+            gt_fixes = env.coord_data[:, :2]
             # save results
-            save_dict['all_preds'].append(np.array(pred_scores, dtype=np.float32))
+            save_dict['score_preds'].append(np.array(pred_scores, dtype=np.float32))
+            save_dict['fix_preds'].append(np.array(pred_fixes, dtype=np.float32))
             save_dict['gt_labels'].append(env.clsID)
+            save_dict['gt_fixes'].append(gt_fixes)
             save_dict['toas'].append(env.begin_accident)
             save_dict['vids'].append(data_info[0, 1])
 
         np.save(result_file, save_dict)
 
     # evaluate the results
-    all_pred = save_dict['all_preds']
-    all_labels = save_dict['gt_labels']
-    all_toas = save_dict['toas']
-    all_fps = [30/args.frame_interval] * len(all_labels)
-    AP, mTTA, TTA_R80 = evaluation_dynamic(all_pred, all_labels, all_toas, all_fps)
+    gt_labels = save_dict['gt_labels']
+    all_fps = [30/cfg.ENV.frame_interval] * len(gt_labels)
+    vis_file = os.path.join(output_dir, 'PRCurve_SAC.png')
+    AP, mTTA, TTA_R80 = evaluation_accident(save_dict['score_preds'], gt_labels, save_dict['toas'], all_fps, draw_curves=True, vis_file=vis_file)
     # print
     print("AP = %.4f, mean TTA = %.4f, TTA@0.8 = %.4f"%(AP, mTTA, TTA_R80))
-
+    mse_fix = evaluation_fixation(save_dict['fix_preds'], save_dict['gt_fixes'])
+    print('Fixation Prediction MSE=%.4f'%(mse_fix))
 
 
 if __name__ == "__main__":
     # parse input arguments
-    args = parse_main_args()
+    cfg = parse_main_args()
 
     # fix random seed 
-    set_deterministic(args.seed)
+    set_deterministic(cfg.seed)
 
-    # gpu options
-    gpu_ids = [int(id) for id in args.gpus.split(',')]
-    os.environ['CUDA_VISIBLE_DEVICES'] = args.gpus
-    device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
-
-    if args.phase == 'train':
+    if cfg.phase == 'train':
         train()
-    elif args.phase == 'test':
+    elif cfg.phase == 'test':
         test()
-    elif args.phase == 'eval':
+    elif cfg.phase == 'eval':
         # evaluate()
         pass
     else:
