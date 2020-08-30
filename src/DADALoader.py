6b05d6ba0b5401159f973de50c959afda1e28e66
baowentao <wtbao2018@gmail.com>
2020-08-30 14:28:17 -0400

    test small data

--- src/DADALoader.py
+++ src/DADALoader.py
@@ -6,13 +6,12 @@ from torch.utils.data import Dataset
 from torchvision import transforms
 
 class DADALoader(Dataset):
-    def __init__(self, root_path, phase, interval=1, min_frames=32, max_frames=-1, 
+    def __init__(self, root_path, phase, interval=1, max_frames=-1, 
                        transforms={'image':None, 'focus': None, 'fixpt': None}, 
                        params_norm=None, binary_cls=False, use_focus=True, use_fixation=True, cls_task=False):
         self.root_path = root_path
         self.phase = phase  # 'training', 'testing', 'validation'
         self.interval = interval
-        self.min_frames = min_frames
         self.max_frames = max_frames
         self.transforms = transforms
         self.params_norm = params_norm
@@ -32,7 +31,7 @@ class DADALoader(Dataset):
 
     def get_data_list(self):
         # video path
-        rgb_path = os.path.join(self.root_path, self.phase, 'rgb')
+        rgb_path = os.path.join(self.root_path, self.phase, 'rgb_videos')
         assert os.path.exists(rgb_path), "Path does not eixst! %s"%(rgb_path)
         # loop for each type of accident
         data_list = []
@@ -42,10 +41,7 @@ class DADALoader(Dataset):
                 continue
             accident_rgb_path = os.path.join(rgb_path, accident)
             for vid in sorted(os.listdir(accident_rgb_path)):
-                num_frames = len(os.listdir(os.path.join(accident_rgb_path, vid)))
-                if num_frames < self.min_frames:  # igore too short videos
-                    continue
-                data_list.append(accident + '/' + vid)
+                data_list.append(accident + '/' + vid.split('.')[0])
         return data_list
 
 
@@ -73,28 +69,28 @@ class DADALoader(Dataset):
         return sel_frames, inds
 
 
-    def read_frames_from_images(self, index, interval=1, max_frames=-1):
-        """ Read video frames
-        """
-        # video path
-        video_path = os.path.join(self.root_path, self.phase, 'rgb', self.data_list[index])
-        assert os.path.exists(video_path), "Path does not exist: %s"%(video_path)
-        frame_ids, video_data = [], []
-        for i, filename in enumerate(sorted(os.listdir(video_path))):  # we must sort the image files to ensure a sequential frame order
-            if i % interval == 0:
-                # get frame id list
-                fid = int(filename.split('.')[0])
-                frame_ids.append(fid)
-                # read video frame
-                im = cv2.imread(os.path.join(video_path, filename))
-                assert im is not None, "Read file failed! %s"%(os.path.join(video_path, filename))
-                im = cv2.cvtColor(im, cv2.COLOR_BGR2RGB)  # RGB: (660, 1584, 3)
-                video_data.append(im)
-        if max_frames > 0:
-            frame_ids, inds = self.select_frames(frame_ids, num=max_frames)
-            video_data = [video_data[i] for i in inds]
-        video_data = np.array(video_data, dtype=np.float32)  # 4D tensor, (N, 660, 1584, 3)
-        return video_data, frame_ids
+    # def read_frames_from_images(self, index, interval=1, max_frames=-1):
+    #     """ Read video frames
+    #     """
+    #     # video path
+    #     video_path = os.path.join(self.root_path, self.phase, 'rgb', self.data_list[index])
+    #     assert os.path.exists(video_path), "Path does not exist: %s"%(video_path)
+    #     frame_ids, video_data = [], []
+    #     for i, filename in enumerate(sorted(os.listdir(video_path))):  # we must sort the image files to ensure a sequential frame order
+    #         if i % interval == 0:
+    #             # get frame id list
+    #             fid = int(filename.split('.')[0])
+    #             frame_ids.append(fid)
+    #             # read video frame
+    #             im = cv2.imread(os.path.join(video_path, filename))
+    #             assert im is not None, "Read file failed! %s"%(os.path.join(video_path, filename))
+    #             im = cv2.cvtColor(im, cv2.COLOR_BGR2RGB)  # RGB: (660, 1584, 3)
+    #             video_data.append(im)
+    #     if max_frames > 0:
+    #         frame_ids, inds = self.select_frames(frame_ids, num=max_frames)
+    #         video_data = [video_data[i] for i in inds]
+    #     video_data = np.array(video_data, dtype=np.float32)  # 4D tensor, (N, 660, 1584, 3)
+    #     return video_data, frame_ids
 
     def read_frames_from_videos(self, index, interval=1, max_frames=-1):
         """Read video frames
@@ -123,27 +119,27 @@ class DADALoader(Dataset):
         return video_data, frame_ids
 
 
-    def read_focus_from_images(self, frame_ids, index):
-        """ Read focus images
-        """
-        # focus path
-        focus_path = os.path.join(self.root_path, self.phase, 'focus', self.data_list[index])
-        assert os.path.exists(focus_path), "Path does not exist: %s"%(focus_path)
-        focus_data = []
-        for fid in frame_ids:
-            focus_file = os.path.join(focus_path, "%04d.png"%(fid))
-            if not os.path.exists(focus_file):
-                focus_file = os.path.join(focus_path, "%04d.jpg"%(fid))
-                if not os.path.exists(focus_file):
-                    print('Focus file does not exist: %s'%(focus_file))
-                    break
-            focus = cv2.imread(focus_file)  # BGR: (660, 1584, 3)
-            if focus.shape[2] != 1:
-                focus = cv2.cvtColor(focus, cv2.COLOR_BGR2GRAY)
-            focus = np.expand_dims(focus, axis=-1)  # (H, W, 1)
-            focus_data.append(focus)
-        focus_data = np.array(focus_data, dtype=np.float32)
-        return focus_data
+    # def read_focus_from_images(self, frame_ids, index):
+    #     """ Read focus images
+    #     """
+    #     # focus path
+    #     focus_path = os.path.join(self.root_path, self.phase, 'focus', self.data_list[index])
+    #     assert os.path.exists(focus_path), "Path does not exist: %s"%(focus_path)
+    #     focus_data = []
+    #     for fid in frame_ids:
+    #         focus_file = os.path.join(focus_path, "%04d.png"%(fid))
+    #         if not os.path.exists(focus_file):
+    #             focus_file = os.path.join(focus_path, "%04d.jpg"%(fid))
+    #             if not os.path.exists(focus_file):
+    #                 print('Focus file does not exist: %s'%(focus_file))
+    #                 break
+    #         focus = cv2.imread(focus_file)  # BGR: (660, 1584, 3)
+    #         if focus.shape[2] != 1:
+    #             focus = cv2.cvtColor(focus, cv2.COLOR_BGR2GRAY)
+    #         focus = np.expand_dims(focus, axis=-1)  # (H, W, 1)
+    #         focus_data.append(focus)
+    #     focus_data = np.array(focus_data, dtype=np.float32)
+    #     return focus_data
 
 
     def read_focus_from_videos(self, frame_ids, index):
@@ -313,11 +309,11 @@ def setup_dataloader(cfg):
     params_norm = {'mean': [0.485, 0.456, 0.406], 'std': [0.229, 0.224, 0.225]}
     # training dataset
     train_data = DADALoader(cfg.data_path, 'training', interval=cfg.frame_interval, max_frames=cfg.max_frames, 
-                            transforms=transform_dict, params_norm=params_norm, binary_cls=cfg.binary_cls)
+                            transforms=transform_dict, params_norm=params_norm, binary_cls=cfg.binary_cls, use_focus=cfg.use_salmap)
     traindata_loader = DataLoader(dataset=train_data, batch_size=cfg.batch_size, shuffle=True, num_workers=cfg.num_workers)
     # validataion dataset
     eval_data = DADALoader(cfg.data_path, 'validation', interval=cfg.frame_interval, max_frames=cfg.max_frames, 
-                            transforms=transform_dict, params_norm=params_norm, binary_cls=cfg.binary_cls)
+                            transforms=transform_dict, params_norm=params_norm, binary_cls=cfg.binary_cls, use_focus=cfg.use_salmap)
     evaldata_loader = DataLoader(dataset=eval_data, batch_size=cfg.batch_size, shuffle=False, num_workers=cfg.num_workers)
     print("# train set: %d, eval set: %d"%(len(train_data), len(eval_data)))
     return traindata_loader, evaldata_loader
@@ -335,7 +331,7 @@ if __name__ == '__main__':
                         help='The relative path of dataset.')
     parser.add_argument('--batch_size', type=int, default=1,
                         help='The batch size in training process. Default: 1')
-    parser.add_argument('--frame_interval', type=int, default=5,
+    parser.add_argument('--frame_interval', type=int, default=1,
                         help='The number of frames per second for each video. Default: 10')
     parser.add_argument('--max_frames', default=-1, type=int,
                         help='Maximum number of frames for each untrimmed video.')
@@ -353,36 +349,27 @@ if __name__ == '__main__':
     device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
 
     # initialize dataset
-    args.image_shape = [660, 1584]
+    args.image_shape = [330, 792]
     args.output_shape = (np.array(args.input_shape) / 8).astype(np.int64)
     args.binary_cls = True
+    args.use_salmap = False
     traindata_loader, evaldata_loader = setup_dataloader(args)
-
-    # prefetcher = PreFetcher(traindata_loader)
-    # video_data, focus_data, coord_data = prefetcher.next()
-    # iteration = 0
-    # while video_data is not None:
-    #     iteration += 1
-    #     video_data, focus_data, coord_data = prefetcher.next()
-    #     print(iteration)
 
     num_frames = []
     num_clsses = 2
     cls_stat = np.zeros((num_clsses,), dtype=np.int64)
     t_start = time.time()
-    for i, (video_data, focus_data, coord_data, data_info) in enumerate(traindata_loader):
+    for i, (video_data, _, coord_data, data_info) in enumerate(traindata_loader):
         clsID = int(coord_data[0, :, 2].unique()[1])
         print("batch: %d / %d, num_frames = %d, cls = %d, time=%.3f"%(i, len(traindata_loader), video_data.shape[1], clsID, time.time() - t_start))
         cls_stat[clsID-1] += 1
         num_frames.append(video_data.shape[1])
         t_start = time.time()
 
-    # np.savez('msg_data_2', frames=num_frames, stats=cls_stat)
-
     fig, ax = plt.subplots()
     plt.bar(range(len(num_frames)), num_frames)
     plt.title('Number of frames for each video')
-    plt.savefig('stat_frames.png')
+    plt.savefig('stat_frames_small.png')
     plt.close()
 
     fig, ax = plt.subplots()
@@ -390,5 +377,5 @@ if __name__ == '__main__':
     # plt.xticks(range(num_clsses), ('ego_person', 'ego_vehicle', 'ego_road', 'nonego_road', 'nonego_vehicle', 'nonego_person'))
     plt.xticks(range(num_clsses), ('ego', 'nonego'))
     plt.title('Number of videos for each category')
-    plt.savefig('stat_two.png')
+    plt.savefig('stat_twocls_small.png')
     plt.close()
